c10a97c42f03df3ec1e00c557c7522fb675afbbc
jonathan <joncanales2@gmail.com>
2023-03-29 14:41:51 -0400

    initial setup for server.

---
+++ server.ts
@@ -0,0 +1,11 @@
+//@ts-nocheck 
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
