d8f5da491a3523dd60dc70166ab5d6f8c0e40bc1
jonathan <joncanales2@gmail.com>
2023-04-06 15:38:46 -0400

    encrypt password upon user creation.

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -7,6 +7,15 @@ import User from "../models/User.ts";
 // @route POST /api/v1/auth/register
 // @access Public
 export const register = asyncHandler(async (req, res, next) => {
-  const {email, password} = req.body
+  const { username, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    username,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({ success: true });
 });
--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,7 @@
 //@ts-nocheck
 import mongoose from "mongoose";
 const { v4: uuidv4 } = require("uuid");
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
   _id: {
@@ -8,15 +9,35 @@ const UserSchema = new mongoose.Schema({
     default: uuidv4,
     required: [true],
   },
-  email: { type: String, required: true, unique: true, match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'please add a valid email' ]},
-  password: { type: String, required: [true, 'please add a password'], minLength: 7, select: false },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "please add a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "please add a password"],
+    minLength: 7,
+    select: false,
+  },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
-  firstName: { type: String, required: [true, 'please add a first name'] },
-  lastName: { type: String, required: [true, 'please add a last name'] },
+  username: { type: String, required: [true, "please add a userName"] },
   image: { type: String },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
   createdAt: { type: Date, default: Date.now },
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+});
+
+// encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", UserSchema);
