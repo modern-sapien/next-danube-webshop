76882330cf2810f96151bb531ee277c4a9cacd2b
jonathan <joncanales2@gmail.com>
2023-04-07 16:12:59 -0400

    basic scaffolding of next.

---
+++ next-webstore/app/about/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>AboutPage</div>
+
+  )
+}
+
+export default AboutPage
---
+++ next-webstore/app/book/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookPage = () => {
+  return (
+    <div>BookPage</div>
+  )
+}
+
+export default BookPage
