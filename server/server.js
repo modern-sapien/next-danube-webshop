4d2d131a43a7999c3e7ac44625e88f04bc4212cc
jonathan <joncanales2@gmail.com>
2023-04-12 11:30:23 -0400

    updating for cors.

--- server/server.js
+++ server/server.js
@@ -40,6 +40,7 @@ app.use(
 // allow origin and credentials
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://next-danube-webshop.vercel.app'");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
