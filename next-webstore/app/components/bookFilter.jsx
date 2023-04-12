7fb4e5499050aa7a4e9f2791386698a92ad65105
jonathan <joncanales2@gmail.com>
2023-04-12 11:16:19 -0400

    updated users routes to handle protect route appropriately, bookfilters and userProfile logic.

--- next-webstore/app/components/bookFilter.jsx
+++ next-webstore/app/components/bookFilter.jsx
@@ -6,44 +6,60 @@ const bookFilter = () => {
       <div className="filter-container">
         <div className="filter-option">
           <h3>Genre</h3>
-          <form>
+          <form data-test="book-filter-genre">
             <label>
               <input type="checkbox" name="filter1" value="1" />
-              Filter 1
+              Romance
             </label>
             <label>
               <input type="checkbox" name="filter2" value="2" />
-              Filter 2
+              Sci-Fi
             </label>
             <label>
               <input type="checkbox" name="filter3" value="3" />
-              Filter 3
+              Fantasy
+            </label>
+            <label>
+              <input type="checkbox" name="filter4" value="4" />
+              Nicholas Cage
+            </label>
+            <label>
+              <input type="checkbox" name="history" value="4" />
+              History
             </label>
           </form>
         </div>
         <div className="filter-option">
           <h3>Review</h3>
-          <form>
+          <form data-test="book-filter-review">
             <label>
               <input type="checkbox" name="filter1" value="1" />
-              has review
+              has reviews
             </label>
           </form>
         </div>
         <div className="filter-option">
           <h3>Rating</h3>
-          <form>
+          <form data-test="book-filter-review">
             <label>
-              <input type="checkbox" name="filter1" value="1" />
-              Filter 1
+              <input type="checkbox" name="filter1" value="5" />
+              5 stars
             </label>
             <label>
-              <input type="checkbox" name="filter2" value="2" />
-              Filter 2
+              <input type="checkbox" name="filter2" value="4" />
+              4 stars
             </label>
             <label>
               <input type="checkbox" name="filter3" value="3" />
-              Filter 3
+              3 stars
+            </label>
+            <label>
+              <input type="checkbox" name="filter3" value="2" />
+              2 stars
+            </label>
+            <label>
+              <input type="checkbox" name="filter3" value="1" />
+              1 star
             </label>
           </form>
         </div>
--- next-webstore/app/components/userProfile.jsx
+++ next-webstore/app/components/userProfile.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserProfile = ({ userData }) => {
+  const [formValues, setFormValues] = useState({
+    _id: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `https://next-danube-webshop-backend.vercel.app/v1/users/${userData._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formValues),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("There was a problem updating the user profile", error);
+    }
+  };
+
   return (
-    <form className="card-expanded">
+    <form className="card-expanded" onSubmit={handleSubmit}>
       <h1>User Profile</h1>
       {userData && (
         <>
@@ -12,7 +47,9 @@ const UserProfile = ({ userData }) => {
               type="text"
               className="form-control"
               id="username"
-              value={userData.data.username}
+              name="username"
+              value={formValues.username || userData.data.username}
+              onChange={handleInputChange}
             />
           </div>
           <div className="form-group">
@@ -21,12 +58,21 @@ const UserProfile = ({ userData }) => {
               type="email"
               className="form-control"
               id="email"
-              value={userData.data.email}
+              name="email"
+              value={formValues.email || userData.data.email}
+              onChange={handleInputChange}
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input type="password" className="form-control" id="password" />
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={formValues.password}
+              onChange={handleInputChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="dateJoined">Date Joined:</label>
@@ -34,7 +80,9 @@ const UserProfile = ({ userData }) => {
               type="text"
               className="form-control"
               id="dateJoined"
+              name="dateJoined"
               value={userData.data.createdAt}
+              readOnly
             />
           </div>
         </>
