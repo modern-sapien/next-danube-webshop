77fb87cbd59ad18392219682be5812e6ed7d0bbe
jonathan <joncanales2@gmail.com>
2023-04-13 00:21:09 -0400

    seeing if router works in production, if not roll back.

--- next-webstore/app/components/bookCards.jsx
+++ next-webstore/app/components/bookCards.jsx
@@ -17,36 +17,41 @@ const BookCards = async () => {
   const books = await fetchBooks();
   return (
     <div className="right-column">
+      {books ? (
+        <>
+          {books.map((book) => (
+            <div className="card" data-test={book.title} key={book.id}>
+              <Link href={`/book/${book.id}`}>
+                <div
+                  data-test="book-link"
+                  style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}
+                >
+                  <h3 style={{ display: "flex", justifyContent: "center" }}>{book.title}</h3>
+                  <img
+                    style={{ display: "flex", justifyContent: "center" }}
+                    src="./gatsbycover.png"
+                    width="200"
+                    height="200"
+                    alt="User profile picture"
+                  />
+                </div>
+              </Link>
 
-{ books ? <>      {books.map((book) => (
-        <div className="card" data-test={book.title} key={book.id}>
-          <Link
-            href="/book"
-            data-test="book-link"
-            style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}
-          >
-            <h3 style={{ display: "flex", justifyContent: "center" }}>{book.title}</h3>
-            <img
-              style={{ display: "flex", justifyContent: "center" }}
-              src="./gatsbycover.png"
-              width="200"
-              height="200"
-              alt="User profile picture"
-            />
-          </Link>
-
-          <ul>
-            <li> Author: {book.author}</li>
-            <li> Publisher: {book.publisher}</li>
-            <li>Genre: {books.genre}</li>
-            <li> Price: {book.price}</li>
-          </ul>
-          <button className="btn" data-test="add-to-cart">
-            add to cart
-          </button>
-        </div>
-      ))}</> : <h1>LOADING</h1> }
-
+              <ul>
+                <li> Author: {book.author}</li>
+                <li> Publisher: {book.publisher}</li>
+                <li>Genre: {books.genre}</li>
+                <li> Price: {book.price}</li>
+              </ul>
+              <button className="btn" data-test="add-to-cart">
+                add to cart
+              </button>
+            </div>
+          ))}
+        </>
+      ) : (
+        <h1>LOADING</h1>
+      )}
     </div>
   );
 };
