65312d3880d2981b2ef18caf9f6111b18d2334b0
jonathan <joncanales2@gmail.com>
2023-04-13 01:43:16 -0400

    updated functionality to retrieve books and no longer try to hit the bookId route and updated userProfile page functionality, so it works appropriately.

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,8 +1,14 @@
 //@ts-nocheck
-const express = require("express") ;
-const Book = require("../models/Book.js") ;
-const { getBook, getBooks, createBook, updateBook, deleteBook } = require("../controllers/books.js") ;
-const advancedResults = require("../middleware/advancedResults.js") ;
+const express = require("express");
+const Book = require("../models/Book.js");
+const {
+  getBook,
+  getBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+} = require("../controllers/books.js");
+const advancedResults = require("../middleware/advancedResults.js");
 
 // Include other resource routers
 const reviewRouter = require("./reviews.js");
@@ -12,7 +18,7 @@ const router = express.Router();
 // Re-route into other resource routers
 router.use("/:bookId/reviews", reviewRouter);
 
-const { protect, authorize } = require( "../middleware/auth.js");
+const { protect, authorize } = require("../middleware/auth.js");
 
 router
   .route("/")
@@ -22,7 +28,7 @@ router
 router
   .route("/:id")
   .get(getBook)
-  .put(protect, authorize("admin"), updateBook)
+  .put(protect, authorize("user", "admin"), updateBook)
   .delete(protect, authorize("admin"), deleteBook);
 
 module.exports = router;
