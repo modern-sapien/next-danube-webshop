18cba886e54523444f63d54b7c8cd5ab0e94dfa7
jonathan <joncanales2@gmail.com>
2023-05-10 20:23:31 -0400

    added -staging back.

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const corsOptions = {
     const allowedOrigins = [
       "http://localhost:3000",
       "https://next-danube-webshop.vercel.app",
+      "https://next-danube-webshop-staging.vercel.app",
       "https://next-danube-webshop-nqycdzdfn-modern-sapien.vercel.app/",
       /^https:\/\/next-danube-webshop(-[a-zA-Z0-9]+)?\.vercel\.app$/
     ];
