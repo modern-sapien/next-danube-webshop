0b0c74cd0d50cccf6e89d73f4d76565f3a87e225
jonathan <joncanales2@gmail.com>
2023-05-14 18:25:31 -0400

    removed scratch files.

--- scratch2.js
+++
@@ -1,9 +0,0 @@
-const regex = /^https:\/\/next-danube-webshop(-[a-zA-Z0-9]+)?\.vercel\.app$/;
-
-const url1 = "https://next-danube-webshop.vercel.app";
-const url2 = "https://next-danube-webshop-abc123.vercel.app";
-const url3 = "https://next-danube-webshop-staging.vercel.app";
-
-console.log(regex.test(url1)); // true
-console.log(regex.test(url2)); // true
-console.log(regex.test(url3)); // true
