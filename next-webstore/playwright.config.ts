04df19b97ab8d6b96ed486753f6c64943cdb814c
jonathan <joncanales2@gmail.com>
2023-05-15 15:21:21 -0400

    updating path for playwright.yml to store artifacts appropriately when running and reduced time for webserver startup.

--- next-webstore/playwright.config.ts
+++ next-webstore/playwright.config.ts
@@ -16,14 +16,14 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
 
   webServer: {
     command: "npm run dev",
     url: baseURL,
-    timeout: 120 * 1000,
+    timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 
