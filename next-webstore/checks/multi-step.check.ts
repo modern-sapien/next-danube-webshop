4821cfd1eafc5532bb03291c54c8ca43f01706eb
jonathan <joncanales2@gmail.com>
2024-01-01 18:04:44 -0500

    added note about adding API key from crudapi.co.uk

--- next-webstore/checks/multi-step.check.ts
+++ next-webstore/checks/multi-step.check.ts
@@ -3,6 +3,11 @@ import { defaults } from '../tests/defaults';
 import { group } from './resources/group.check';
 import * as path from 'path';
 
+/**
+* This CHECK will fail if you do not set obtain an API key from this free service
+* Use this third party service to create a simple CRUD API
+* An API_KEY from https://crudapi.co.uk/ is required for this example to work.
+*/
 new MultiStepCheck(`next-danube-${defaults.projectEnv}-crud`, {
   name: `Next Danube ${defaults.projectEnv} CRUD - multi`,
   group,
