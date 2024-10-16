2eac5082644c589ab55b02ad83b3011de01e0333
jonathan <joncanales2@gmail.com>
2024-10-16 11:10:24 -0400

    Initializing new branch to refine strategy for env handling.

--- next-webstore/tests/defaults.ts
+++ next-webstore/tests/defaults.ts
@@ -22,30 +22,3 @@ export const defaults = {
       ? 'https://next-danube-webshop-backend.vercel.app/api/v1'
       : 'https://next-danube-webshop-backend-staging.vercel.app/api/v1',
 };
-
-// export const defaults = {
-//   projectEnv: projectEnv,
-//   apiKey: process.env.CHECKLY_API_KEY,
-//   // Use NODE_ENV to set time defaults in one line
-//   testTime: ['production', 'staging'].includes(process.env.NODE_ENV) ? 45000 : 60000,
-//   waitTime: ['production', 'staging'].includes(process.env.NODE_ENV) ? 2500 : 3000,
-//   scheduleTime: projectEnv: 'production' ? 5 : 30,
-//   // Conditional user object
-//   testUser: {
-//     email: projectEnv === 'production' ? 'jane@example.com' : 'staging-sam@example.com',
-//     password: projectEnv === 'production' ? 'password2' : 'staging-password1',
-//   }
-//   // Page URL based on environment with default fallback
-//   pageUrl: {
-//     projectEnv === 'production'
-//       ? 'https://next-danube-webshop.vercel.app'
-//       : projectEnv === 'staging'
-//       ? 'https://next-danube-webshop-staging.vercel.app'
-//       : process.env.NEXT_PUBLIC_ENVIRONMENT_URL || '',
-
-//   // API URL with similar pattern
-//   apiUrl:
-//     projectEnv === 'production'
-//       ? 'https://next-danube-webshop-backend.vercel.app/api/v1'
-//       : 'https://next-danube-webshop-backend-staging.vercel.app/api/v1',
-// };
